00c73dce59531c1b032528674711911fa1222249
manjushasoji <manjushajoy@hotmail.com>
2020-08-15 19:26:02 -0500

    Bean config - Java based and xml

---
+++ src/main/java/manj/springframework/jokes/jokesapp/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package manj.springframework.jokes.jokesapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+//@Configuration
+public class ChuckConfiguration {
+
+	//@Bean
+	public ChuckNorrisQuotes chuckNorrisQuotes() {
+
+		return new ChuckNorrisQuotes();
+	}
+
+}
